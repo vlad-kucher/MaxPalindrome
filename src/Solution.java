8b59ee6c5ca72df408567e49f440f71514833495
Vlad <vlad.kucher.92@gmail.com>
2017-11-13 14:35:10 +0200

    refactoring

--- src/Solution.java
+++ src/Solution.java
@@ -4,69 +4,48 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        //получаем все простые числа меньше 100_000
-        List<Integer> primes = getPrimes();
-
-        //удаляем простые числа в которых меньше 5 цифр
-        primes.removeIf(p -> p < 10000);
-
-        //ищем максимальный палиндом для каждого простого числа
-        long[] maxPalindromesForEachPrime = getMaxPalindromesForEachPrime(primes);
-
-        //выводим результат
-        int index = getIndexOfMaxPalindrome(maxPalindromesForEachPrime);
-        System.out.println("Max palindrome: " + maxPalindromesForEachPrime[index] + " = " + primes.get(index) +
-                " * " + maxPalindromesForEachPrime[index]/primes.get(index));
+        List<Integer> primes = getFiveDigitPrimes();
+        findMaxPalindrome(primes);
     }
 
-    private static List<Integer> getPrimes(){
+    private static List<Integer> getFiveDigitPrimes(){
         List<Integer> primes = new ArrayList<>();
 
-        label: for (int i = 2; i < 100000; i++) {
-            for (int p : primes) {
-                if (i%p == 0) {
-                    continue label;
+        outer: for (int i = 2; i < 100000; i++) {
+            for (int prime : primes) {
+                if (i%prime == 0) {
+                    continue outer;
                 }
             }
             primes.add(i);
         }
 
+        primes.removeIf(prime -> prime < 10000);
         return primes;
     }
 
-    private static long[] getMaxPalindromesForEachPrime(List<Integer> primes) {
-        long[] palindromes = new long[primes.size()];
+    private static void findMaxPalindrome(List<Integer> primes) {
+        long maxPalindrome = 0;
+        int multiplier = 0;
 
-        label: for (int i = primes.size()-1; i >= 0 ; i--) {
+        outer: for (int i = primes.size()-1; i >= 0 ; i--) {
             for (int j = i-1; j >= 0 ; j--) {
                 long n = (long) primes.get(i) * primes.get(j);
                 if (checkPalindrome(n)) {
-                    palindromes[i] = n;
-                    continue label;
+                    if (maxPalindrome < n) {
+                        maxPalindrome = n;
+                        multiplier = primes.get(i);
+                    }
+                    continue outer;
                 }
             }
         }
 
-        return palindromes;
-    }
-
-    private static int getIndexOfMaxPalindrome(long[] palindromes) {
-        int maxIndex = 0;
-        long maxElem = 0;
-
-        for (int i = 0; i < palindromes.length; i++) {
-            if (palindromes[i]>maxElem) {
-                maxElem = palindromes[i];
-                maxIndex = i;
-            }
-        }
-
-        if (maxElem == 0) {
+        if (maxPalindrome == 0) {
             System.out.println("No palindromes found");
-            System.exit(0);
+        } else {
+            System.out.format("Max palindrome: %d = %d * %d", maxPalindrome, multiplier, maxPalindrome/multiplier);
         }
-
-        return maxIndex;
     }
 
     private static boolean checkPalindrome(long number) {
